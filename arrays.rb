5d15a7af555255e065a7387666b62ccebfefaab1
jake08bennett <jake08bennett@hotmail.com>
2015-09-20 16:17:30 -0600

    Arrays to starry

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,69 @@
 module Arrays
 	
+	def middles(arr1, arr2)
+		[arr1[1], arr2[1]]
+	end
+
+	def max(max_array)
+		max = 0
+		max_array.each do |element|
+			if element > max 
+				max = element
+			end
+		end
+		max
+	end	
+
+	def lucky13?(arr)
+		sum = 0
+		arr.each do |n|
+			if n == 1 || n == 3
+				sum += 1
+			end
+		end
+		if sum == 0
+			true
+		else
+			false
+		end
+	end
+
+	def repeat_separator(word, separator, count)
+		string = ""
+		(2..count).each do |n|
+			string += word + separator
+		end
+		string += word 
+		string
+	end
+
+	def same_ends?(array, range)
+		string1 = ""
+		string2 = ""
+		(0...range).each do |n|
+			string1 += array[n].to_s
+		end
+		(array.length - range...array.length).each do |n|
+			string2 += array[n].to_s
+		end
+		string1 == string2
+	end
+
+	def starry(string)
+		string_array = []
+		string = string.downcase
+		(0...string.length).each do |n|
+			if string[n] == "*"
+				string_array.push(string[n-1] + string[n] + string[n + 1])
+			end
+		end
+		(0...string_array.length).each do |n|
+			if(string_array[n][0] != string_array[n][2])
+				return false
+			end
+		end
+		true
+	end
 	
 	
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -11,4 +11,36 @@ class ArraysTest < MiniTest::Test
    	end.new
 	end
 
+	def test_middles
+		assert_equal [4, 2], @m.middles([22, 4, 9], [3, 2, 33])
+		assert_equal [7855, 2], @m.middles([2, 7855, 53], [0, 2, 66])
+	end
+
+	def test_max
+		assert_equal 43, @m.max([43, 5, 21, 12, 7])
+		assert_equal 105, @m.max([105, 12, 17, 21])
+	end
+	
+	def test_lucky13?
+		assert_equal true, @m.lucky13?([2, 0, 4, 28, 6])
+		assert_equal false, @m.lucky13?([1, 4, 8,])
+		assert_equal false, @m.lucky13?([3, 6, 5,])
+		assert_equal false, @m.lucky13?([3, 2, 1])
+	end
+	
+	def test_repeat_separator
+		assert_equal "JakeBanterJake", @m.repeat_separator("Jake", "Banter", 2)
+		assert_equal "heyAndheyAndhey", @m.repeat_separator("hey", "And", 3)
+	end
+
+	def test_same_ends?
+		assert_equal true, @m.same_ends?([3, 4, 5, 6, 3, 4], 2)
+		assert_equal true, @m.same_ends?([1, 2, 2, 3, 4], 0)
+		assert_equal false, @m.same_ends?([2, 3, 4, 5, 1, 2], 3)
+	end
+
+	def test_starry
+		assert_equal true, @m.starry("saa*Abs")
+		assert_equal false, @m.starry("saa*bas")
+	end
 end
