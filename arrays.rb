b8f1864649cfd788276196282aa1e97bcd1754f9
jake08bennett <jake08bennett@hotmail.com>
2015-09-20 19:02:38 -0600

    Finished These Stupid Arrays

--- arrays.rb
+++ arrays.rb
@@ -64,6 +64,57 @@ module Arrays
 		end
 		true
 	end
-	
-	
+
+	def plus_it(string, token)
+		new_string = "+" * string.length
+		token_index = 0 
+		while true 
+			offset = token_index == 0 ? 0 :  token_index + token.length 
+			token_index = string.index(token, offset)
+			if token_index == nil 
+				return new_string
+			else
+				for i in 0...token.length 
+					new_string[token_index + i] = token[i]
+				end
+			end
+			
+		end
+	end
+
+	def zero_front(arr)
+		new_arr = []
+		for element in 0...arr.length
+			if arr[element] == 0
+				new_arr.push(arr[element])
+			end
+		end
+		for element in 0...arr.length
+			if arr[element] > 0 
+				new_arr.push(arr[element])
+			end
+		end
+		new_arr
+	end
+
+	def ten_run(ten_array)
+		multiple = 0
+		for element in 0...ten_array.length
+			multiple = ten_array[element] if ten_array[element] % 10 == 0
+			ten_array[element] = multiple if multiple > 0
+		end
+		ten_array
+	end
+
+	def max_span(max_span_array)
+		span = 0 
+		for n in 0...max_span_array.length 
+			for x in n...max_span_array.length 
+				if max_span_array[x] == max_span_array[n]
+					span = (1 +(x-n)) if (1+(x-n)) > span && x-n >0
+				end
+			end
+		end
+		return span
+	end
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -43,4 +43,31 @@ class ArraysTest < MiniTest::Test
 		assert_equal true, @m.starry("saa*Abs")
 		assert_equal false, @m.starry("saa*bas")
 	end
+
+	def test_plus_it
+		assert_equal "+++ABC+++" , @m.plus_it("123ABC456", "ABC")
+		assert_equal "++++++" , @m.plus_it("123456", 'a')
+		assert_equal "+++abc+++abc" , @m.plus_it("123abc123abc", 'abc')
+	end
+
+	def test_zero_front
+		assert_equal [5,3,1,6], @m.zero_front([5,3,1,6])
+		assert_equal [0,0,0,0,4,5,6,7,10], @m.zero_front([4,0,0,5,6,7,0,10,0])
+		assert_equal [0,0,5,3,1,6], @m.zero_front([5,0,3,0,1,6])
+		assert_equal [0,0,0,0,0], @m.zero_front([0,0,0,0,0])
+	end
+
+	def test_ten_run
+		assert_equal [2, 10, 10, 10, 20, 20], @m.ten_run([2, 10, 3, 4, 20, 5])
+		assert_equal [10,20,30,40,50], @m.ten_run([10,20,30,40,50])
+		assert_equal [1,2,3,4,5,6], @m.ten_run([1,2,3,4,5,6])
+		assert_equal [2,10,10,10,30,30], @m.ten_run([2,10,67,84,30,45])
+	end
+
+	def test_max_span
+		assert_equal 6, @m.max_span([1,4,2,1,4,1,4])
+		assert_equal 6, @m.max_span([1,4,2,1,4,4,4])
+		assert_equal 4 , @m.max_span([1,2,1,1,3])
+   		assert_equal 0, @m.max_span([1,2,3,4,5,6])
+	end
 end
